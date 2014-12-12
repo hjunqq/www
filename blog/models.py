9d573955188766b16ab6c0c5d36a23e1b5df68ba
huijun <qhjun@live.com>
2014-12-12 10:26:27 +0800

    构建后台 Signed-off-by: huijun <qhjun@live.com>

--- blog/models.py
+++ blog/models.py
@@ -4,20 +4,7 @@ from django.contrib.auth.models import (BaseUserManager, AbstractBaseUser)
 
 # Create your models here. userprofile
 class BlogGroup(models.Model): 
-    """ Groups are a generic way of categorizing users 
-    to apply permissions, or some other label, to those 
-    users. A user can belong to any number of groups. A 
-    user in a group automatically has all the permissions 
-    granted to that group. For example, if the group Site 
-    editors has the permission can_edit_home_page, any 
-    user in that group will have that permission. Beyond 
-    permissions, groups are a convenient way to categorize 
-    users to apply some label, or extended functionality, 
-    to them. For example, you could create a group 'Special 
-    users', and you could write code that would do special 
-    things to those users -- such as giving them access to 
-    a members-only portion of your site, or sending them 
-    members-only email messages. """ 
+    """  """ 
     GroupName = models.CharField(max_length=80, unique=True) 
     Permissions = models.ManyToManyField(Permission, verbose_name='permissions', blank=True) 
     objects = GroupManager() 
